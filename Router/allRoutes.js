ebb30cb8de46e45aae872e68230ea36987184921
Hamid2024 <hamidmahamat04@gmail.com>
2024-11-27 13:10:31 +0100

    add singup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 export const allRoutes = [
  new Route("/", "Accueil", "/pages/home.html"),
  new Route("/Galerie", "La galerie", "/pages/galerie.html"),
+ new Route("signin", "Connexion", "/pages/signin.html"),
+ new Route("singup", "Inscription", "/pages/singup.html"),
 ];
  
 //Le titre s'affiche comme ceci : Route.titre - websitename

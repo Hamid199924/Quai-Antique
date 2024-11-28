5b9aed52856baaab87ae98bc129cac52635be16a
Hamid2024 <hamidmahamat04@gmail.com>
2024-11-28 17:55:57 +0100

    Gestion d'affichages de reservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,12 +4,10 @@ export const allRoutes = [
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassworde", "Changement de mot de passe", "/pages/auth/editPassworde.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
 
-allRoutes.forEach(route => {
-    console.log(route.path);
- });
- 

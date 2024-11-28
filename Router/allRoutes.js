484dfc8317a11a4696da844844db81587b806bd2
Hamid2024 <hamidmahamat04@gmail.com>
2024-11-28 11:52:26 +0100

    finish des fichier account password singnin singup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,11 +1,15 @@
-import Route from "./Route.js";
-//Définir ici vos routes
 export const allRoutes = [
- new Route("/", "Accueil", "/pages/home.html"),
- new Route("/Galerie", "La galerie", "/pages/galerie.html"),
- new Route("signin", "Connexion", "/pages/signin.html"),
- new Route("singup", "Inscription", "/pages/singup.html"),
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/Galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
 ];
- 
+
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
+
+allRoutes.forEach(route => {
+    console.log(route.path);
+ });
+ 
